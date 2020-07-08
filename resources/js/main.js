5cd94033c1e3afa8b73b97a8a6382bdcd23d4ed7
Daniel Shires <daniel.shires@btinternet.com>
2020-07-08 13:38:20 +1000

    1.0

---
+++ resources/js/main.js
@@ -0,0 +1,182 @@
+var quotes = [
+  ["The faker you are, the more succesfull you can be.", '<a href="https://www.youtube.com/watch?v=bEg5ySTUGxE" target="_blank">Pablo Stanley</a>'],
+  [
+    "Fuck the midtones",
+    '<a href="https://www.youtube.com/watch?v=8McHbjaxpbA" target="_blank">Gerhard Steidl</a>',
+  ],
+  [
+    "Thinking is hard. Not thinking is harder",
+    '<a href="https://hansaarsman.nl" target="_blank">Hans Aarsman</a>',
+  ],
+  [
+    "Perfect is the enemy of the good. Free yourself from the tyranny of perfection! Never surrender!",
+    '<a href="https://www.erikkessels.com" target="_blank">Erik Kessels</a>',
+  ],
+  [
+    "Live in the leading—the spaces in between the rules.",
+    '<a href="http://www.stefanmumaw.com" target="_blank">Stefan Mumaw</a>',
+  ],
+  [
+    "Have no fear of perfection—you’ll never reach it.",
+    '<a href="https://en.wikipedia.org/wiki/Salvador_Dal%C3%AD" target="_blank">Salvador Dali</a>',
+  ],
+  [
+    "Good design is like a refrigerator—when it works, no one notices, but when it doesn’t, it sure stinks.",
+    '<a href="https://ireneau.com" target="_blank">Irene Au</a>',
+  ],
+  [
+    "Digital design is like painting, except the paint never dries.",
+    '<a href="https://brody-associates.com" target="_blank">Neville Brody</a>',
+  ],
+  [
+    "Design is not just what it looks like and feels like. Design is how it works.",
+    '<a href="https://en.wikipedia.org/wiki/Steve_Jobs" target="_blank">Steve Jobs</a>',
+  ],
+  [
+    "Creativity is a highfalutin word for the work I have to do between now and Tuesday.",
+    '<a href="https://en.wikipedia.org/wiki/Ray_Kroc" target="_blank">Ray Kroc</a>',
+  ],
+  [
+    "Art is anything you can get away with",
+    '<a href="https://en.wikipedia.org/wiki/Marshall_McLuhan" target="_blank">Marshal McLuhan</a>',
+  ],
+  [
+    "It’s through mistakes that you actually can grow. You have to get bad in order to get good",
+    '<a href="https://www.pentagram.com/about/paula-scher" target="_blank">Paula Scher</a>',
+  ],
+  [
+    "Get rid of everything that is not essential to making a point.",
+    '<a href="https://www.christophniemann.com" target="_blank">Christoph Niemann</a>',
+  ],
+  [
+    "The role of the designer is that of a good, thoughtful host anticipating the needs of his guests.",
+    '<a href="https://en.wikipedia.org/wiki/Charles_Eames" target="_blank">Charles Eames</a>',
+  ],
+  [
+    "Design can be art. Design can be simple. That’s why it’s so complicated.",
+    '<a href="https://www.paulrand.design" target="_blank">Paul Rand</a>',
+  ],
+  [
+    "People ignore designs that ignore people",
+    '<a href="https://frankchimero.com" target="_blank">Frank Chimero</a>',
+  ],
+  [
+    "You don’t have to be ‘a creative’ to be creative",
+    '<a href="https://frankchimero.com" target="_blank">Drory Ben-Menachem</a>',
+  ],
+  [
+    "I love deadlines. I like the whooshing sound they make as they fly by.",
+    '<a href="https://en.wikipedia.org/wiki/Douglas_Adams" target="_blank">Douglas Adams</a>',
+  ],
+  [
+    "For every complex problem, there is an answer that is clear, simple, and wrong.",
+    '<a href="https://en.wikipedia.org/wiki/H._L._Mencken" target="_blank">H. L. Mencken</a>',
+  ],
+  [
+    "Good design is honest.",
+    '<a href="https://en.wikipedia.org/wiki/Dieter_Rams" target="_blank">Dieter Rams</a>',
+  ],
+  [
+    "Design is thinking made visual",
+    '<a href="https://en.wikipedia.org/wiki/Saul_Bass" target="_blank">Saul Bass</a>',
+  ],
+  [
+    "How well we communicate is determined not by how well we say things, but how well we are understood.",
+    '<a href="https://en.wikipedia.org/wiki/Andrew_Grove" target="_blank">Charles Eames</a>',
+  ],
+  [
+    "Good design is obvious. Great design is transparent.",
+    '<a href="https://joesparano.com/about" target="_blank">Joe Sparano</a>',
+  ],
+  [
+    "Where do new ideas come from? The answer is simple: differences. Creativity comes from unlikely juxtapositions.",
+    '<a href="https://en.wikipedia.org/wiki/Nicholas_Negroponte" target="_blank">Nicholas Negroponte</a>',
+  ],
+  [
+    "Do not seek praise. Seek criticism.",
+    '<a href="https://en.wikipedia.org/wiki/Paul_Arden" target="_blank">Paul Arden</a>',
+  ],
+  [
+    "Styles come and go. Good design is a language, not a style.",
+    '<a href="https://en.wikipedia.org/wiki/Massimo_Vignelli" target="_blank">Massimo Vignelli</a>',
+  ],
+  [
+    "You don’t think your way to creative work. You work your way to creative thinking.",
+    '<a href="https://www.hermanmiller.com/en_au/designers/nelson/" target="_blank">George Nelson</a>',
+  ],
+  [
+    "If I had asked people what they wanted, they would have said faster horses",
+    '<a href="https://en.wikipedia.org/wiki/Henry_Ford" target="_blank">Henry Ford</a>',
+  ],
+  [
+    "Design is a solution to a problem. Art is a question to a problem.",
+    '<a href="https://en.wikipedia.org/wiki/John_Maeda" target="_blank">Henry Ford</a>',
+  ],
+  [
+    "There is no design without discipline. There is no discipline without intelligence.",
+    '<a href="https://en.wikipedia.org/wiki/Massimo_Vignelli" target="_blank">Massimo Vignelli</a>',
+  ],
+  [
+    "Design everything on the assumption that people are not heartless or stupid but marvelously capable, given the chance.",
+    '<a href="https://en.wikipedia.org/wiki/John_Chris_Jones" target="_blank">John Chris Jones</a>',
+  ],
+  [
+    "Perfection is achieved not when there is nothing more to add, but when there is nothing left to take away",
+    '<a href="https://en.wikipedia.org/wiki/Antoine_de_Saint-Exupéry" target="_blank">Antoine de Saint-Exupery</a>',
+  ],
+  [
+    "There is no such thing as a boring project. There are only boring executions.",
+    '<a href="https://www.siegelvision.com/ireneetzkorn" target="_blank">Irene Etzkorn</a>',
+  ],
+  [
+    "The time it takes to make a decision increases as the number of alternatives increases.",
+    '<a href="https://en.wikipedia.org/wiki/W._E._Hick" target="_blank">William Edmund Hick</a>',
+  ],
+  [
+    "Sometimes when you innovate, you make mistakes. It is best to admit them quickly, and get on with improving your other innovations.",
+    '<a href="https://en.wikipedia.org/wiki/Steve_Jobs" target="_blank">Steve Jobs</a>',
+  ],
+  [
+    "Simplicity is not the goal. It is the by-product of a good idea and modest expectations.",
+    '<a href="https://www.paulrand.design" target="_blank">Paul Rand</a>',
+  ],
+  [
+    "Simplicity, wit and good typography",
+    '<a href="https://www.pentagram.com/about/michael-bierut" target="_blank">Michael Beruit</a>',
+  ],
+  [
+    "Practice safe design: Use a concept",
+    '<a href="http://www.35k.com" target="_blank">Petrula Vrontikis</a>',
+  ],
+  [
+    "Practice safe design: Use a concept",
+    '<a href="http://www.35k.com" target="_blank">Petrula Vrontikis</a>',
+  ]
+];
+
+
+// On page load get quotes
+window.onload = function () {
+  randomquote = Math.floor(Math.random() * 1);
+  currentQuote = quotes[randomquote][0];
+  currentAuthor = quotes[randomquote][1];
+  document.getElementById("quotetext").innerHTML = currentQuote;
+  document.getElementById("quotesource").innerHTML = currentAuthor;
+};
+
+// function functionName 
+function getQuote() {
+  randomquote = Math.floor(Math.random() * quotes.length);
+  currentQuote = quotes[randomquote][0];
+  currentAuthor = quotes[randomquote][1];
+  document.getElementById("quotetext").innerHTML = currentQuote;
+  document.getElementById("quotesource").innerHTML = currentAuthor;
+
+  $(document).ready(function () {
+    var colors = ["#E831A0", "#0C82EA", "#02978C", "01388B", "#FD6835", "#CB3338", "#2CA7DB", "#652DCC", "#009646", "#017DA6", "#B62CD1"];
+    var rand = Math.floor(Math.random() * colors.length);
+    $('.background-color').css("background-color", colors[rand]);
+    $('button.mobile-button-random').css("color", colors[rand]);
+    event.preventDefault();
+  });
+}
